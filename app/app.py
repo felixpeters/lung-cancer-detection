02034db837a40f630705a75c8a3a96746ecc9d01
Felix Peters <felix@felixpeters.me>
2021-02-11 11:52:44 +0100

    save scan meta as dataframe, not series

--- app/app.py
+++ app/app.py
@@ -1,5 +1,24 @@
+import os
+from pathlib import Path
+
 import streamlit as st
+import pandas as pd
+
+DATA_DIR = Path(__file__).absolute().parent / "data"
+
+@st.cache
+def load_meta():
+    scan_df = pd.read_csv(DATA_DIR / "scan_meta.csv")
+    nod_df = pd.read_csv(DATA_DIR / "nodule_meta.csv")
+    return scan_df, nod_df
+
+def load_ct_img():
+    return
+
+scan_df, nod_df = load_meta()
 
 st.header("Lung cancer detection")
 
-st.write("Coming soon...")
+st.write(scan_df.shape)
+
+st.write(nod_df)
