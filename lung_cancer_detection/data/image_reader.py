4cc84e8ea7cf6e5a7d0e8a070cd147caa25b729f
Felix Peters <felix@felixpeters.me>
2021-03-15 09:59:23 +0100

    extract basic meta data from scan meta

--- lung_cancer_detection/data/image_reader.py
+++ lung_cancer_detection/data/image_reader.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Union, Sequence, Tuple
+from typing import Union, Sequence, Tuple, Dict, Any
 
 import pandas as pd
 import numpy as np
@@ -64,5 +64,35 @@ class LIDCReader(ImageReader):
         meta = self.meta_df.loc[pat_id]
         return (img, meta)
 
-    def get_data(self, img: Tuple[np.ndarray, pd.Series]):
-        return
+    def get_data(self, img: Tuple[np.ndarray, pd.Series]) -> Tuple[np.ndarray, Dict[str, Any]]:
+        """Extract data array and meta data from loaded image and return them.
+
+        Args:
+            img (Tuple[np.ndarray, pd.Series]): Loaded image (typically output from `read` method)
+
+        Returns:
+            Tuple[np.ndarray, Dict[str, Any]]: Numpy array of image data and dict of meta data.
+        """
+        img_array, scan_meta = img
+        meta = self._get_meta_dict(scan_meta)
+        meta["original_affine"] = self._get_affine(scan_meta)
+        meta["affine"] = meta["original_affine"].copy()
+        meta["spatial_shape"] = self._get_spacial_shape(img_array)
+        return img_array, meta
+
+    def _get_meta_dict(self, img_meta: pd.Series) -> Dict[str, Any]:
+        """Extract ITK-compatible meta data from image meta.
+
+        Args:
+            img_meta (pd.Series): Image meta data (typically returned from `read` method)
+
+        Returns:
+            Dict[str, Any]: Meta data dictionary with keys origina, spacing and direction.
+        """
+        meta = {}
+        meta["origin"] = np.asarray(
+            [float(i.strip()) for i in img_meta.ImagePositionPatient[1:-1].split(",")])
+        meta["spacing"] = np.asarray(
+            [img_meta.PixelSpacing, img_meta.PixelSpacing, img_meta.SliceSpacing])
+        meta["direction"] = np.eye(3)
+        return meta
