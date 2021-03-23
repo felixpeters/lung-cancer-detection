cb345dfb320f3017b69fa594b12d148406d65ef3
Felix Peters <felix@felixpeters.me>
2021-03-23 15:11:45 +0100

    add more test data

--- lung_cancer_detection/data/test_data_module.py
+++ lung_cancer_detection/data/test_data_module.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+import numpy as np
 
 from .data_module import LIDCDataModule
 
@@ -24,16 +25,16 @@ def test_init(data_module):
 
 def test_setup(data_module):
     data_module.setup()
-    assert len(data_module.train_ds) == 4
-    assert len(data_module.val_ds) == 1
+    assert len(data_module.train_ds) == 8
+    assert len(data_module.val_ds) == 2
     item = data_module.train_ds[0]
     img = item["image"].numpy()
     label = item["label"].numpy()
     affine = item["image_meta_dict"]["affine"]
     assert list(img.shape) == [1, 180, 180, 90]
     assert list(label.shape) == [1, 180, 180, 90]
-    assert img.min() == 0.0
-    assert img.max() == 1.0
+    assert np.all(img >= 0.0) == True
+    assert np.all(img <= 1.0) == True
     assert affine[0, 0] == 1.5
     assert affine[1, 1] == 1.5
     assert affine[2, 2] == 2.0
@@ -42,7 +43,7 @@ def test_setup(data_module):
 def test_train_dataloader(data_module):
     data_module.setup()
     loader = data_module.train_dataloader()
-    assert len(loader) == 2
+    assert len(loader) == 4
     for batch in loader:
         assert batch["image"].numpy().shape == (2, 1, 180, 180, 90)
         assert batch["label"].numpy().shape == (2, 1, 180, 180, 90)
@@ -53,5 +54,5 @@ def test_val_dataloader(data_module):
     loader = data_module.val_dataloader()
     assert len(loader) == 1
     for batch in loader:
-        assert batch["image"].numpy().shape == (1, 1, 180, 180, 90)
-        assert batch["label"].numpy().shape == (1, 1, 180, 180, 90)
+        assert batch["image"].numpy().shape == (2, 1, 180, 180, 90)
+        assert batch["label"].numpy().shape == (2, 1, 180, 180, 90)
