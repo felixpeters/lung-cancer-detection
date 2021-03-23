98d7c2ee94d79a6aa7ba0a3cf3f67f9b05f9d28a
Felix Peters <felix@felixpeters.me>
2021-03-23 09:20:18 +0100

    add init function of data module

--- lung_cancer_detection/data/data_module.py
+++ lung_cancer_detection/data/data_module.py
@@ -1,6 +1,19 @@
 from typing import Optional
+from pathlib import Path
 
 import pytorch_lightning as pl
+from monai.transforms import (
+    AddChanneld,
+    Compose,
+    LoadImaged,
+    ScaleIntensityd,
+    Spacingd,
+    ToTensord,
+    CenterSpatialCropd,
+)
+from monai.data import Dataset, PersistentDataset
+from monai.utils import set_determinism
+import pandas as pd
 
 from image_reader import LIDCReader
 
@@ -12,14 +25,46 @@ class LIDCDataModule(pl.LightningDataModule):
             - https://github.com/felixpeters/melanoma-detection/blob/master/src/data/data_module.py
     """
 
-    def __init__(self, data_dir):
+    def __init__(self, data_dir: Path, cache_dir: Path, batch_size: int, val_split: float = 0.2, sample=None, seed: int = 47):
         super().__init__()
         self.data_dir = data_dir
+        self.cache_dir = cache_dir
+        self.batch_size = batch_size
+        self.val_split = val_split
+        self.sample = sample
+        self.seed = seed
+        reader = LIDCReader(data_dir)
+        self.train_transform = Compose([
+            LoadImaged(keys=["image", "label"], reader=reader),
+            AddChanneld(keys=["image", "label"]),
+            Spacingd(keys=["image", "label"], pixdim=(
+                1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+            # TODO: Test different scaling methods
+            ScaleIntensityd(keys=["image"]),
+            # TODO: Test different cropping methods
+            CenterSpatialCropd(keys=["image", "label"],
+                               roi_size=[180, 180, 90]),
+            # TODO: Test data augmentation methods
+            ToTensord(keys=["image", "label"]),
+        ])
+        self.val_transforms = Compose([
+            LoadImaged(keys=["image", "label"], reader=reader),
+            AddChanneld(keys=["image", "label"]),
+            Spacingd(keys=["image", "label"], pixdim=(
+                1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+            ScaleIntensityd(keys=["image"]),
+            CenterSpatialCropd(keys=["image", "label"],
+                               roi_size=[180, 180, 90]),
+            ToTensord(keys=["image", "label"]),
+        ])
 
     def prepare_data(self):
         return
 
     def setup(self, stage: Optional[str] = None):
+        self.scans = pd.read_csv(
+            self.data_dir/"meta/scans.csv", index_col="PatientID")
+
         return
 
     def train_dataloader(self):
