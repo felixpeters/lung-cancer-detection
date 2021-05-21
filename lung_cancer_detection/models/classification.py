718d32a2b3e6d1493a215ff131a3eb7b8581e9f9
Felix Peters <felix@felixpeters.me>
2021-05-21 11:37:10 +0200

    add metrics to nodule classification model

--- lung_cancer_detection/models/classification.py
+++ lung_cancer_detection/models/classification.py
@@ -19,8 +19,8 @@ class NoduleClassificationDenseNet(pl.LightningModule):
         self.lr = lr
         self.train_acc = torchmetrics.Accuracy()
         self.val_acc = torchmetrics.Accuracy()
-        self.train_auc = torchmetrics.AUROC()
-        self.val_auc = torchmetrics.AUROC()
+        self.train_auc = torchmetrics.AUROC(num_classes=2)
+        self.val_auc = torchmetrics.AUROC(num_classes=2)
         self.save_hyperparameters()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -31,15 +31,25 @@ class NoduleClassificationDenseNet(pl.LightningModule):
         y = y.squeeze().type(torch.LongTensor)
         output = self(x)
         loss = F.cross_entropy(output, y)
+        logits = F.softmax(output)
         self.log("train_loss", loss)
+        self.train_acc(logits, y)
+        self.log("train_acc", self.train_acc)
+        self.train_auc(logits, y)
+        self.log("train_auc", self.train_auc)
         return {"loss": loss}
 
     def validation_step(self, batch: Dict, batch_idx: int) -> torch.Tensor:
         x, y = batch["image"], batch["label"]
         y = y.squeeze().type(torch.LongTensor)
         output = self(x)
+        logits = F.softmax(output)
         loss = F.cross_entropy(output, y)
         self.log("val_loss", loss)
+        self.val_acc(logits, y)
+        self.log("val_acc", self.val_acc)
+        self.val_auc(logits, y)
+        self.log("val_auc", self.val_auc)
         return {"val_loss": loss}
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
